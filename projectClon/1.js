4c08f4eee14e34e030bd25059336a5e8d8016ef8
Unknown <narutofan68@mail.ru>
2018-05-23 13:32:35 +0300

    clone

---
+++ projectClon/1.js
@@ -0,0 +1,18 @@
+// var num = -123456789;
+// function reverseInt(num){
+//     var result = 0;
+//     if(num < 0){
+//         return num * (-1);
+//     }
+//     while(num > 0) {
+//             result = result * 10 + num % 10;
+//             num = Math.floor(num / 10);
+//             console.log(result);
+//     }
+//     return result;
+// }
+//  console.log(reverseInt(num));
+
+function sum (num){
+    return a + b;
+}
---
+++ projectClon/photo.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="shortcut icon" href="http://www.brentstirton.com/favicon.ico">
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed|Sorts+Mill+Goudy" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<title>Photojournalism 	&mdash; Brent Stirton</title>
+        <style>
+            .logoImage {
+                width: 150px;
+                max-width: 140px;
+                margin-top: 50px;
+                margin-left: 50px;
+            }
+
+            ul {
+                -webkit-margin-before: 1em;
+                -webkit-margin-after: 1em;
+                -webkit-margin-start: 0px;
+                -webkit-margin-end: 0px;
+                -webkit-padding-start: 44px;
+                list-style-type: none;
+
+            }
+            a {
+                font-family: "Sorts Mill Goudy";
+                line-height: 1.4em;
+                text-transform: none;
+                letter-spacing: 0px;
+                font-weight: 400;
+                font-style: normal;
+                font-size: 15px;
+                display: block;
+                padding: .2em;
+                color: #929090;
+                margin-left: 7px;
+            }
+            a:hover {
+                color: crimson;
+                text-decoration:none;
+            }
+            li {
+                font-family: "proxima-nova";
+                line-height: 1.6em;
+                text-transform: none;
+                letter-spacing: 0px;
+                font-weight: 400;
+                font-style: normal;
+            }
+            .mainNavigation {
+                margin-top: 30px;
+                font-weight: bold;
+            }
+            .secondaryNavigation {
+                margin-top: 20px;
+            }
+
+            .header {
+                margin-top: 0px;
+                position: fixed;
+            }
+            .container {
+                margin-top: 7px;
+                margin-left: 10px;
+                padding: 0px;
+                left: 210px;
+                top: 30px;
+                display: block;
+            }
+            .galleryItem1 {
+                position: absolute;
+                top: 0px;
+                left: 0px;
+            }
+            .galleryItem2 {
+                position: absolute;
+                top: 0px;
+                left: 408px;
+            }
+            .galleryItem3 {
+                position: absolute;
+                top: 0px;
+                left: 816px;
+            }
+            .galleryItem4 {
+                position: absolute;
+                top: 0px;
+                left: 1224px;
+            }
+            .galleryItem5 {
+                position: absolute;
+                top: 275px;
+                left: 0px;
+            }
+            .galleryItem6 {
+                position: absolute;
+                top: 275px;
+                left: 408px;
+            }
+            .galleryItem7 {
+                position: absolute;
+                top: 275px;
+                left: 816px;
+            }
+            .galleryItem8 {
+                position: absolute;
+                top: 275px;
+                left: 1224px;
+            }
+            .galleryItem9 {
+                position: absolute;
+                top: 550px;
+                left: 0px;
+            }
+            .galleryItem10 {
+                position: absolute;
+                top: 550px;
+                left: 408px;
+            }
+            .galleryItem11 {
+                position: absolute;
+                top: 578px;
+                left: 816px;
+            }
+            .galleryItem12 {
+                position: absolute;
+                top: 550px;
+                left: 1224px;
+            }
+            .galleryItem13 {
+                position: absolute;
+                top: 825px;
+                left: 0px;
+            }
+            .galleryItem14 {
+                position: absolute;
+                top: 825px;
+                left: 408px;
+            }
+            .galleryItem15 {
+                position: absolute;
+                top: 853px;
+                left: 816px;
+            }
+            .galleryItem16 {
+                position: absolute;
+                top: 838px;
+                left: 1224px;
+            }
+
+        </style>
+	</head>
+	<body>
+        <div class="header">
+            <img class="logoImage" src="http://static1.squarespace.com/static/55f8f7e9e4b065c11d2e4b1b/t/57aac657d2b857917b1cdb86/1511360168312/?format=750w">
+            <nav class="mainNavigation">
+                <ul>
+                    <li class="galleryCollection">
+                        <a href="/photo.php">Photojournalism</a>
+                    </li>
+                    <li class="galleryCollection">
+                        <a href="/portraits.html">Portraits</a>
+                    </li>
+                    <li class="galleryCollection">
+                        <a href="/travel.html">Travel</a>
+                    </li>
+                    <li class="galleryCollection">
+                        <a href="/project.html">Projects</a>
+                    </li>
+                    <li class="galleryCollection">
+                        <a href="/motion.html">Motion</a>
+                    </li>
+                </ul>
+            </nav>
+            <nav class="secondaryNavigation">
+                <ul>
+                    <li class="pageColection">
+                        <a href="/bio.html">Bio</a>
+                    </li>
+                    <li class="pageCollection">
+                        <a href="/contact.html">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-6 container" style="width: 1633px; height: 1220px">
+            <div class="galleryItem1" data-type="image">
+                <a href="#">
+                    <img src="image/Photojn_0001.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem2" data-type="image">
+                <a href="#">
+                    <img src="image/Photojn_0002.jpg" width="398" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem3" data-type="image">
+                <a href="#">
+                    <img src="image/Photojn_0003.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem4" data-type="image">
+                <a href="#">
+                    <img src="image/Photojn_0004.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem5" data-type="image">
+                <a href="#">
+                    <img src="image/Photojn_0006.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem6" data-type="image">
+                <a href="#">
+                    <img src="image/Photojn_0005.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem7" data-type="image">
+                <a href="#">
+                    <img src="image/Photojn_0007.jpg" width="398px" height="293px">
+                </a>
+            </div>
+            <div class="galleryItem8" data-type="image">
+                <a href="#">
+                    <img src="image/add_001.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem9" data-type="image">
+                <a href="#">
+                    <img src="image/Phtjn035.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem10" data-type="image">
+                <a href="#">
+                    <img src="image/addphotojn_001.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem11" data-type="image">
+                <a href="#">
+                    <img src="image/addphotojn_003.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem12" data-type="image">
+                <a href="#">
+                    <img src="image/addphotojn_002.jpg" width="399px" height="278px">
+                </a>
+            </div>
+            <div class="galleryItem13" data-type="image">
+                <a href="#">
+                    <img src="image/addphotojn_004.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem14" data-type="image">
+                <a href="#">
+                    <img src="image/addphotojn_005.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem15" data-type="image">
+                <a href="#">
+                    <img src="image/addphotojn_007.jpg" width="398px" height="265px">
+                </a>
+            </div>
+            <div class="galleryItem16" data-type="image">
+                <a href="#">
+                    <img src="image/addphotojn_006.jpg" width="398px" height="265px">
+                </a>
+            </div>
+        </div>
+	</body>
+</html>
